0f286d567b818efa791719ef79fe32dc801dfa71
Kent Tokunaga <kenttokunaga@gmail.com>
2018-11-11 14:00:39 -0500

    Provide progress to user

--- cmds/start.js
+++ cmds/start.js
@@ -1,8 +1,47 @@
-const { TOKEN } = require('../config');
+const inquirer = require('inquirer');
+const ora = require('ora');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const spinner = ora();
 
-const start = (token) => {
+const { TOKEN } = require('../config/config');
+const questions = [
+  {
+    type: 'confirm',
+    name: 'askAgain',
+    message: 'Continue?',
+    default: true
+  }
+];
+
+const ask = () => {
+  inquirer.prompt(questions).then(answers => {
+    if (answers.askAgain) {
+      ask();
+    } else {
+      console.log('Logging out...');
+      client.destroy()
+        .then(() => process.exit(0))
+        .catch(err => {
+          console.error(err);
+          process.exit(1);
+        });
+    }
+  });
+};
+
+const start = async token => {
+  spinner.start('Logging in');
   token = token || TOKEN;
-  console.log(token);
+
+  try {
+    await client.login(token);
+    spinner.succeed();
+    ask();
+  } catch (e) {
+    spinner.fail('Unable to log in');
+    process.exit(1);
+  }
 };
 
 module.exports = start;
