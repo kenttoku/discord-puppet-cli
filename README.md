b27e7c685a77959126a78299c051560440372310
Kent Tokunaga <kenttokunaga@gmail.com>
2018-11-11 15:15:49 -0500

    Get channels from client

---
+++ README.md
@@ -0,0 +1,10 @@
+# Puppet Bot CLI
+
+A simple cli for controlling your own bot
+
+## Usage
+
+Install using npm 
+```
+npm install
+```
--- cmds/start.js
+++ cmds/start.js
@@ -14,20 +14,69 @@ const questions = [
   }
 ];
 
-const ask = () => {
-  inquirer.prompt(questions).then(answers => {
-    if (answers.askAgain) {
-      ask();
-    } else {
-      console.log('Logging out...');
-      client.destroy()
-        .then(() => process.exit(0))
-        .catch(err => {
-          console.error(err);
-          process.exit(1);
-        });
-    }
-  });
+const test = [];
+for (let i=0; i<100; i++) {
+  test.push(`test${i}`);
+}
+
+const guildQuestions = guilds => ([
+  {
+    type: 'list',
+    name: 'guild',
+    message: 'Select a server',
+    choices: guilds
+  }
+]);
+
+const getGuilds = async client => {
+  spinner.start('Finding servers');
+  const guilds = await client.guilds.array().map(guild => ({
+    name: `${guild.name} (ID: ${guild.id})`,
+    value: { id: guild.id, name: guild.name }
+  }));
+
+  if (!guilds.length) {
+    spinner.fail();
+    console.error('Please invite your bot to a server');
+    process.exit(0);
+  }
+
+  spinner.succeed();
+  return guilds;
+};
+
+const getChannels = async(client, guildId) => {
+  spinner.start('Finding channels');
+  const guild = await client.guilds.find(guild => guild.id === guildId);
+  const channels = guild.channels.array()
+    .filter(channel => channel.type === 'text')
+    .map(channel => ({
+      name: `${channel.name} (ID: ${channel.id})`,
+      value: { id: channel.id, name: channel.name }
+    }));
+
+  spinner.succeed();
+  return channels;
+};
+
+const ask = async client => {
+  // Prompt user For guild (server)
+  const guilds = await getGuilds(client);
+  const { guild } = await inquirer.prompt(guildQuestions(guilds));
+
+  // Prompt user for channel
+  const channels = await getChannels(client, guild.id);
+  console.log(channels);
+
+  const answers = await inquirer.prompt(questions);
+  if (answers.askAgain) {
+    ask();
+  } else {
+    spinner.start('Logging out');
+    await client.destroy();
+    spinner.succeed();
+    process.exit(0);
+  }
 };
 
 const start = async token => {
@@ -37,7 +86,7 @@ const start = async token => {
   try {
     await client.login(token);
     spinner.succeed();
-    ask();
+    ask(client);
   } catch (e) {
     spinner.fail('Unable to log in');
     process.exit(1);
