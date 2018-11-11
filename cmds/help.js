89bcd34bed838aac03f77b9d7e323a6633f4b7a2
Kent Tokunaga <kenttokunaga@gmail.com>
2018-11-10 23:45:28 -0500

    Add help and version commands

---
+++ cmds/help.js
@@ -0,0 +1,15 @@
+const menus = {
+  main: `
+    puppet-bot [command] <options>
+
+    version ............ show package version
+    help ............... show help menu for a command`
+};
+
+module.exports = (args) => {
+  const subCmd = args._[0] === 'help'
+    ? args._[1]
+    : args._[0];
+
+  console.log(menus[subCmd] || menus.main);
+};
--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-module.exports = () => {
-  console.log('Welcome to Puppet bot CLI');
+const minimist = require('minimist');
+
+// Commands
+const version = require('./cmds/version');
+const help = require('./cmds/help');
+
+const puppet = () => {
+  const args = minimist(process.argv.slice(2));
+  let cmd = args._[0] || 'help';
+  if (args.version || args.v) {
+    cmd = 'version';
+  }
+
+  switch (cmd) {
+    case 'version':
+      version();
+      break;
+    case 'help':
+      help(args);
+      break;
+    default:
+      console.error(`"${cmd}" is not a valid command`);
+      break;
+  }
 };
+
+module.exports = puppet;
